f01189f2e6f4f19297ce211602d32d82e1030750
gbrolo <gabrolo13@gmail.com>
2018-09-24 15:09:08 -0600

    Added scrollwindow for chat messages

--- client.c
+++ client.c
@@ -2,7 +2,9 @@
 
 int main(int argc, char *argv[]) {
 
-  GtkWidget *window, *vbox, *vMainBox, *vFriendsBox, *hInputBox, *menubar, *fileMenu, *aboutMenu, *fileMi, *quitMi, *aboutMi, *helpMi, *sendBtn, *chatEntry, *statusCombo, *hBox, *friendsLabel;
+  GtkWidget *window, *vbox, *vMainBox, *vFriendsBox, *hInputBox, *menubar, *fileMenu, *aboutMenu, *fileMi, *quitMi, *aboutMi, *helpMi, *sendBtn, *chatEntry, *statusCombo, *hBox, *friendsLabel, *messagesScrollWindow, *vChatBox, *msg;
+  char buffer[32];
+  int i, j;
 
   gtk_init(&argc, &argv);
 
@@ -11,12 +13,20 @@ int main(int argc, char *argv[]) {
   gtk_window_set_default_size(GTK_WINDOW(window), 600, 400);
   gtk_window_set_title(GTK_WINDOW(window), "Chactter");
 
+  messagesScrollWindow = gtk_scrolled_window_new (NULL, NULL);
+  gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (messagesScrollWindow),
+                                    GTK_POLICY_AUTOMATIC, GTK_POLICY_ALWAYS);
+  
+
   vbox = gtk_vbox_new(FALSE, 0);
   vMainBox = gtk_vbox_new(FALSE, 0);
+  vChatBox = gtk_vbox_new(FALSE, 0);
   vFriendsBox = gtk_vbox_new(FALSE, 0);
   hInputBox = gtk_hbox_new(FALSE, 0);
   hBox = gtk_hbox_new(FALSE, 0);
   gtk_container_add(GTK_CONTAINER(window), vbox);
+
+  gtk_scrolled_window_add_with_viewport(GTK_SCROLLED_WINDOW(messagesScrollWindow), vChatBox);
 
   menubar = gtk_menu_bar_new();
   fileMenu = gtk_menu_new();
@@ -48,12 +58,22 @@ int main(int argc, char *argv[]) {
   gtk_box_pack_start(GTK_BOX(vbox), hBox, FALSE, FALSE, 0);
   gtk_box_pack_start(GTK_BOX(hBox), vMainBox, FALSE, FALSE, 0);
   gtk_box_pack_start(GTK_BOX(hBox), vFriendsBox, FALSE, FALSE, 0);
+  gtk_box_pack_start(GTK_BOX(vMainBox), messagesScrollWindow, TRUE, TRUE, 0);
   gtk_box_pack_start(GTK_BOX(vMainBox), hInputBox, FALSE, FALSE, 0);
 
   gtk_box_pack_start(GTK_BOX(hInputBox), chatEntry, TRUE, TRUE, 0);
   gtk_box_pack_start(GTK_BOX(hInputBox), sendBtn, TRUE, TRUE, 0);
   gtk_box_pack_start(GTK_BOX(hInputBox), statusCombo, TRUE, TRUE, 0);
   gtk_box_pack_start(GTK_BOX(vFriendsBox), friendsLabel, FALSE, FALSE, 0);
+
+  for (i = 0; i < 10; i++) {
+    for (j = 0; j < 10; j++){
+      sprintf(buffer, "Message (%d,%d)\n", i, j);
+      msg = gtk_label_new(buffer);
+      gtk_box_pack_start(GTK_BOX(vChatBox), msg, FALSE, FALSE, 0);
+    }
+  }
+
 
   g_signal_connect(G_OBJECT(window), "destroy",
         G_CALLBACK(gtk_main_quit), NULL);
